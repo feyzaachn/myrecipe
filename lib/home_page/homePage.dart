bae3554a7633593052fa7351d491340a0866d3e4
feyza <feyzaa.chn@gmail.com>
2022-03-30 23:47:29 +0300

    29.03.2022

--- lib/home_page/homePage.dart
+++ lib/home_page/homePage.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:myrecipe/home_page/userProfile.dart';
-import 'package:myrecipe/login/loginPage.dart';
-import 'package:myrecipe/login/sharedPrefs.dart';
-import 'package:myrecipe/services/signOutGoogle.dart';
 import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
 import 'package:circular_bottom_navigation/tab_item.dart';
 
@@ -71,17 +68,8 @@ class _HomePageState extends State<HomePage> {
     }
     return GestureDetector(
       child: Page(selectedColor, selectedPos),
-      onTap: () {
-        print(_navigationController!.value);
-        if (_navigationController!.value == tabItems.length - 1) {
-          _navigationController!.value = 0;
-        } else {
-          _navigationController!.value = (_navigationController!.value! + 1);
-        }
-      },
     );
   }
-
   Widget bottomNav() {
     return CircularBottomNavigation(
       tabItems,
@@ -107,84 +95,206 @@ class _HomePageState extends State<HomePage> {
 Widget Page(Color selectedColor, int position) {
   if (position == 3) {
     return Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              //2 button
-              Container(
-                width: double.infinity,
-                height: 50,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    FloatingActionButton(
-                      onPressed: () {},
-                      elevation: 0,
-                      backgroundColor: Colors.transparent,
-                      child: Icon(Icons.settings,size: 30,color: Colors.black,),
+      width: double.infinity,
+      height: double.infinity,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            //2 button
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {},
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    child: Icon(
+                      Icons.settings,
+                      size: 30,
+                      color: Colors.black,
                     ),
-                    ElevatedButton(
-                        onPressed: () {},
-                       style: ButtonStyle(
-                         backgroundColor: MaterialStateProperty.all(Colors.cyan),
-                         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                           RoundedRectangleBorder(
-                             borderRadius: BorderRadius.circular(22),
-                           )
-                         ),
-                       ),
-                        child: Text("Profili düzenle"),
-                    )
-                  ],
-                ),
-              ),
-              //Image
-              Container(
-                width: double.infinity,
-                height: 190,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.cyan),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(22),
-                        color: Colors.white,
-                        border: Border.all(color: Colors.cyan ,width: 3),
-                        image: DecorationImage(
-                          image: AssetImage("assets/profileImage/default_profile_image.jpg")
-                        )
-                      ),
-                      height: 170,
-                      width: 170,
-                      padding: EdgeInsets.all(0),
-                    )
-                  ],
-                ),
-
+                      )),
+                    ),
+                    child: Text("Profili düzenle"),
+                  )
+                ],
               ),
-              Row(
+            ),
+            //Image
+            Container(
+              width: double.infinity,
+              height: 190,
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    height: 100,
-                    width:370,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(22),
-                      color: Colors.white,
-                      border: Border.all(color: Colors.cyan ,width: 1),
+                        borderRadius: BorderRadius.circular(22),
+                        color: Colors.white,
+                        border: Border.all(color: Colors.cyan, width: 3),
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/profileImage/default_profile_image.jpg"))),
+                    height: 170,
+                    width: 170,
+                    padding: EdgeInsets.all(0),
+                  )
+                ],
+              ),
+            ),
+            //İsim
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Feyza Cihan",
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            //Hakkımda
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 100,
+                  width: 370,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(22),
+                    color: Colors.white,
+                    border: Border.all(color: Colors.cyan, width: 1),
+                  ),
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    "Profilime hoşgeldiniz.",
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            //İletişim
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+              Column(
+                children: [
+                  //E-mail
+                  Container(
+                    width: 200,
+                    child: Row(
+                      children: [
+                        FloatingActionButton(
+                          onPressed: () {},
+                          child: Image.asset("assets/icon/email.png"),
+                          backgroundColor: Colors.transparent,
+                          mini: true,
+                        ),
+                        Text("feyzaa.chn@gmail.com",style: TextStyle(fontSize: 10),),
+                      ],
                     ),
-                    padding: EdgeInsets.all(8),
-                    child: Text("Profilime hoşgeldiniz.",),
+                  ),
+                  //Telefon
+                  Container(
+                    width: 200,
+                    child: Row(
+                      children: [
+                        FloatingActionButton(
+                          onPressed: () {},
+                          child: Image.asset("assets/icon/phone.png"),
+                          backgroundColor: Colors.transparent,
+                          mini: true,
+                        ),
+                        Text("05418638103",style: TextStyle(fontSize: 10)),
+                      ],
+                    ),
+                  ),
+                  //instagram
+                  Container(
+                    width: 200,
+                    child: Row(
+                      children: [
+                        FloatingActionButton(
+                          onPressed: () {},
+                          child: Image.asset("assets/icon/instagram.png"),
+                          backgroundColor: Colors.transparent,
+                          mini: true,
+                        ),
+                        Text("feyzaachn",style: TextStyle(fontSize: 10)),
+                      ],
+                    ),
                   ),
                 ],
               ),
-
-            ],
-          ),
-        ));
+              Column(
+                children: [
+                  //twitter
+                  Container(
+                    width: 200,
+                    child: Row(
+                      children: [
+                        FloatingActionButton(
+                          onPressed: () {},
+                          child: Image.asset("assets/icon/twitter.png"),
+                          backgroundColor: Colors.transparent,
+                          mini: true,
+                        ),
+                        Text("feyzaachn",style: TextStyle(fontSize: 10)),
+                      ],
+                    ),
+                  ),
+                  //facebook
+                  Container(
+                    width: 200,
+                    child: Row(
+                      children: [
+                        FloatingActionButton(
+                          onPressed: () {},
+                          child: Image.asset("assets/icon/facebook.png"),
+                          backgroundColor: Colors.transparent,
+                          mini: true,
+                        ),
+                        Text("feyzaachn",style: TextStyle(fontSize: 10)),
+                      ],
+                    ),
+                  ),
+                  //youtube
+                  Container(
+                    width: 200,
+                    child: Row(
+                      children: [
+                        FloatingActionButton(
+                          onPressed: () {},
+                          child: Image.asset("assets/icon/youtube.png"),
+                          backgroundColor: Colors.transparent,
+                          mini: true,
+                        ),
+                        Text("Feyza Cihan",style: TextStyle(fontSize: 10)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+            SizedBox(height: 20,),
+            Text("Tariflerim"),
+            Divider(height: 3,color: Colors.cyan,),
+          ],
+        ),
+      ),
+    );
   } else {
     return Container(
       width: double.infinity,
